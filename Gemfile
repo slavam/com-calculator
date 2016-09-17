c6df617a76a1fe185c32bca8bcedb522d3b1a8a9
slavam1 <mwm1955@gmail.com>
2016-09-17 19:49:19 +0000

    Adding for tariffs

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'haml'
+gem 'react-rails', '~> 1.0'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/tariffs_controller.rb
+++ app/controllers/tariffs_controller.rb
@@ -7,6 +7,40 @@ class TariffsController < ApplicationController
     @tariffs = Tariff.all.order(:category_id, :name)
   end
   
+  def new
+    @tariff = Tariff.new
+  end
+  
+  
+  def create
+    @tariff = Tariff.new(tariff_params)
+
+    respond_to do |format|
+      if @tariff.save
+        format.html { redirect_to :tariffs, notice: 'tariff was successfully created.' }
+        format.json { render :show, status: :created, location: @tariff }
+      else
+        format.html { render :new }
+        format.json { render json: @tariff.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    respond_to do |format|
+      if @tariff.update(tariff_params)
+        format.html { redirect_to :tariffs, notice: 'tariff was successfully updated.' }
+        format.json { render :show, status: :ok, location: @tariff }
+      else
+        format.html { render :edit }
+        format.json { render json: @tariff.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
   private
     def set_tariff
       @tariff = Tariff.find(params[:id])
